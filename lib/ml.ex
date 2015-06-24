134a2ecfe2d961bbe5ffdeb78030be034cc05537
Batou99 <lorenzo.lopez@intec.es>
2015-06-24 09:25:15 +0000

    Add ML module

---
+++ lib/ml.ex
@@ -0,0 +1,29 @@
+defmodule ML do
+  def find_decrypt_key(text) do
+    load_dict
+    { key, cost } = 0..25 
+      |> Enum.map( fn(n) -> { n, find_cost(text,n) } end ) 
+      |> Enum.min_by( fn({ key, cost }) -> cost end)
+    key
+  end
+
+  def decrypt(text) do
+    key = find_decrypt_key(text)
+    IO.puts Cesar.decrypt(text, key)
+  end
+
+  def find_cost(text, key) do
+    cryptext = Cesar.decrypt(text, key)
+    words = String.split(cryptext, " ")
+    # Total words - found words
+    # The lower the number the more amount of words found
+    Enum.count(words) - Dictionary.count(words)
+  end
+
+  def load_dict do
+    Dictionary.start_link
+    WordlistLoader.load_from_files
+  end
+
+
+end
