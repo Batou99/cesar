9772d0e880561bbf616a7bebbebef220fa1dde81
Batou99 <lorenzo.lopez@intec.es>
2015-06-24 10:05:32 +0000

    Add #ml for autofinding key

--- lib/reader.ex
+++ lib/reader.ex
@@ -1,11 +1,21 @@
 defmodule Reader do
 
   def encrypt(file, key) do
-    process(file,key,&Cesar.encrypt/2)
+    process(file, key, &Cesar.encrypt/2)
   end
 
   def decrypt(file, key) do
-    process(file,key,&Cesar.decrypt/2)
+    process(file, key, &Cesar.decrypt/2)
+  end
+
+  def ml(file) do
+    test_string = File.stream!(file) 
+                  |> Enum.take(5) 
+                  |> Enum.map(&String.strip/1) 
+                  |> Enum.join(" ") 
+                  |> String.replace(~r/[".,]/,"")
+    key = ML.find_decrypt_key(test_string)
+    decrypt(file, key)
   end
 
   defp process(file, key, function) do
